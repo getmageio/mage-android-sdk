371e5bd0e45fa469af3323daa18271210b4e4d1d
Benedict Seyer <benedict.seyer@countr.de>
2020-09-01 15:24:20 +0200

    adding tests

--- Mage/src/androidTest/java/io/getmage/android/ExampleInstrumentedTest.java
+++ Mage/src/androidTest/java/io/getmage/android/ExampleInstrumentedTest.java
@@ -1,15 +1,20 @@
 package io.getmage.android;
 
 import android.content.Context;
+import android.util.Log;
 
+import androidx.test.filters.SmallTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.HashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
@@ -18,11 +23,12 @@ import static org.junit.Assert.*;
  * http://junit.sourceforge.net/javadoc/org/junit/Assert.html
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
-    @Before
-    public void setOptions(){
+    @BeforeClass
+    public static void setOptions(){
         try {
             // This is an API key just for the SDK demo. (Real API keys look different!)
             Mage.setOptions(InstrumentationRegistry.getInstrumentation().getTargetContext(), new HashMap<String, Object>() {{
@@ -30,31 +36,43 @@ public class ExampleInstrumentedTest {
                 put("isStrict", false);
                 put("isProduction", true);
             }});
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Test
     public void canLoadAProduct() {
-        String someProduct = Mage.getIdFromProductName("io.getmage.demo_app.premium_plus", "io.getmage.demo_app.premium_plus_1_25");
-        assertNotSame(someProduct, "io.getmage.demo_app.premium_plus_1_25");
+        Log.d("MageSDKTEST", "loadproduct");
+        String someProduct = Mage.getIdFromProductName("premium_plus", "io.getmage.demo_app.premium_plus_1_25");
+        assertNotSame(someProduct, "io.getmage.demo_app.premium_plus");
     }
 
     @Test
     public void fallsBackToDefaultProduct() {
-        String someProduct = Mage.getIdFromProductName("io.getmage.demo_app.NOTEXISTING", "io.getmage.demo_app.premium_plus_1_25");
+        Log.d("MageSDKTEST", "testfallback");
+        String someProduct = Mage.getIdFromProductName("NOTEXISTING", "io.getmage.demo_app.premium_plus_1_25");
         assertEquals(someProduct, "io.getmage.demo_app.premium_plus_1_25");
     }
 
     @Test
     public void canLookupAProductNameViaAnIapId() {
-        String someProductName = Mage.getProductNameFromId("io.getmage.demo_app.premium_plus_1_25");
-        assertEquals(someProductName, "io.getmage.demo_app.premium_plus");
+        Log.d("MageSDKTEST", "lookupname");
+        String someProductName = Mage.getProductNameFromId("io.getmage.demo_app.premium_plus");
+        assertEquals(someProductName, "premium_plus");
     }
 
     @Test
     public void willReturnNullDuringAProductNameLookupWithANoneExistingId() {
+        Log.d("MageSDKTEST", "wrongID");
         String someProductName = Mage.getProductNameFromId("io.getmage.demo_app.NOTEXISTING");
         assertEquals(someProductName, null);
     }
